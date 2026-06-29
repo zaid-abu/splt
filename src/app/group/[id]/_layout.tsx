e60159ad1ad8c86c561c15cccf29b9171026fa1f
Abu Zaid <abuzaid@Abus-MacBook-Air.local>
2026-06-29 21:25:03 +0530

    refactor: remove add-expense modal screen from group layout

--- src/app/group/[id]/_layout.tsx
+++ src/app/group/[id]/_layout.tsx
@@ -5,10 +5,7 @@ export default function GroupLayout(): JSX.Element {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen
-        name="add-expense"
-        options={{ presentation: "modal" }}
-      />
+
     </Stack>
   );
 }
